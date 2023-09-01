658723ca9f4a6efb80f6376585ce3926530a3f0b
Bhoomi0207 <bhoomiag0207@gmail.com>
2023-09-01 11:52:21 +0530

    admin app

---
+++ app/src/main/java/com/example/adminapp/activity/allorderActivity.kt
@@ -0,0 +1,12 @@
+package com.example.adminapp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.adminapp.R
+
+class allorderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState:Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_allorder)
+    }
+}
